36421cc54a1ffae06e15581e92308d2c9d19f645
= <=>
2022-03-07 05:17:29 -0500

    Controller of sellers and clients

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,3 +2,5 @@ export * from './ping.controller';
 export * from './client-seller.controller';
 export * from './seller-client.controller';
 export * from './seller-user-seller.controller';
+export * from './client.controller';
+export * from './seller.controller';
